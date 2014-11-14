5edd2c9a5d106bd53c9db17022419123c1abf0f4
unknown <smirnovmb@SmirnovMB.svgc.ru>
2014-11-14 13:21:48 +0400

    1. Add to TBaseMDIForm private field FFormFactory. 2. Add abstract Factory method CreateFormFactory which descendants should override and create own FormFactory 3. Override constructor and call CreateFormFactory method inside.

--- Forms/SMBBaseMDIForm.pas
+++ Forms/SMBBaseMDIForm.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, SMBBaseForm, Vcl.Menus, System.Actions,
-  Vcl.ActnList;
+  Vcl.ActnList, SMBFormFactory;
 
 type
   TBaseMDIForm = class(TSMBBaseForm)
@@ -14,10 +14,40 @@ type
     N2: TMenuItem;
     N3: TMenuItem;
     alMain: TActionList;
+  private
+    FFormFactory: TSMBFormFactory;
+  protected
+    function CreateFormFactory(): TSMBFormFactory; virtual; abstract;
+    procedure CreateAndShowMDIChild(aName: String);
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
   end;
 
 implementation
 
 {$R *.dfm}
+
+{ TBaseMDIForm }
+
+constructor TBaseMDIForm.Create(AOwner: TComponent);
+begin
+  inherited;
+  FFormFactory := CreateFormFactory;
+end;
+
+procedure TBaseMDIForm.CreateAndShowMDIChild(aName: String);
+var
+  Form: TForm;
+begin
+  Form := FFormFactory.createForm(aName, Self);
+  Form.Show;
+end;
+
+destructor TBaseMDIForm.Destroy;
+begin
+  FreeAndNil(FFormFactory);
+  inherited;
+end;
 
 end.
