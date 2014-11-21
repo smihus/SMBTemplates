921430398d54fecb2bb6faf85dfecc277f65b8d5
smihus <mike-smirnoff@yandex.ru>
2014-11-21 15:14:34 +0400

    Added property User to SMBBaseMDIForm which get from windows User name. Refactored SMBBaseMDIChild.

--- Forms/SMBBaseMDIChild.pas
+++ Forms/SMBBaseMDIChild.pas
@@ -4,8 +4,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, SMBBaseForm, Vcl.StdCtrls, Vcl.ExtCtrls,
-  SMB.Model, SMB.Controller;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, SMBBaseForm, Vcl.StdCtrls, Vcl.ExtCtrls;
 
 type
   TSMBBaseMDIChildClass = class of TSMBBaseMDIChild;
@@ -15,19 +14,6 @@ type
     bnClose: TButton;
     procedure bnCloseClick(Sender: TObject);
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
-  private
-    FModel: TModel;
-    FController: TController;
-    function GetModel: TModel;
-    function GetController: TController;
-  protected
-    function CreateModel: TModel; virtual;
-    function CreateController: TController; virtual;
-  public
-    constructor Create(AOwner: TComponent); override;
-    destructor Destroy; override;
-    property Model: TModel read GetModel;
-    property Controller: TController read GetController;
   end;
 
 implementation
@@ -40,50 +26,10 @@ begin
   Close;
 end;
 
-constructor TSMBBaseMDIChild.Create(AOwner: TComponent);
-begin
-  inherited;
-  FModel      := CreateModel;
-  FController := CreateController;
-end;
-
-function TSMBBaseMDIChild.CreateController: TController;
-begin
-  Result := nil;
-end;
-
-function TSMBBaseMDIChild.CreateModel: TModel;
-begin
-  Result := nil;
-end;
-
-destructor TSMBBaseMDIChild.Destroy;
-begin
-  if Assigned(FModel) then
-    FreeAndNil(FModel);
-  inherited;
-end;
-
 procedure TSMBBaseMDIChild.FormClose(Sender: TObject; var Action: TCloseAction);
 begin
   inherited;
   Action := caFree;
-end;
-
-function TSMBBaseMDIChild.GetController: TController;
-begin
-  if not Assigned(FModel) then
-    raise Exception.Create('К форме "' + Self.Name + '" не подключен контроллер!!! Обратитесь к разработчику.')
-  else
-    Result := FController;
-end;
-
-function TSMBBaseMDIChild.GetModel: TModel;
-begin
-  if not Assigned(FModel) then
-    raise Exception.Create('К форме "' + Self.Name + '" не подключена модель!!! Обратитесь к разработчику.')
-  else
-    Result := FModel;
 end;
 
 end.
--- Forms/SMBBaseMDIForm.pas
+++ Forms/SMBBaseMDIForm.pas
@@ -11,15 +11,40 @@ type
     procedure FormShow(Sender: TObject);
   private
     FFormManager: IFormManager;
+    FUser: string;
+    function GetUserFromWindows: string;
   public
+    constructor Create(AOwner: TComponent); override;
     procedure SetFormManager(const Value: IFormManager);
+    property User: String read FUser;
   end;
 
 implementation
 
+uses
+  Winapi.Windows;
+
 {$R *.dfm}
 
 { TBaseMDIForm }
+
+function TBaseMDIForm.GetUserFromWindows: string;
+var
+  UserName    : string;
+  UserNameLen : DWord;
+begin
+  UserNameLen := 255;
+  SetLength(UserName, UserNameLen);
+  if GetUserName(PChar(UserName), UserNameLen) then
+    Result := Copy(UserName,1,UserNameLen - 1)
+  else
+    Result := 'Unknown';
+end;
+constructor TBaseMDIForm.Create(AOwner: TComponent);
+begin
+  inherited;
+  FUser := GetUserFromWindows;
+end;
 
 procedure TBaseMDIForm.FormShow(Sender: TObject);
 begin
