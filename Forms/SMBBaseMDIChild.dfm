838bcfcda2cb4100ed68064ae6d12d79410510a8
smihus <mike-smirnoff@yandex.ru>
2014-11-26 16:42:51 +0400

    Moved to TSMBBaseMDIChild: 1. MainMenu 2. PopupMenu 3. ActionList. Added some virtual methods: 1. DoCloseForm 2. DoCreateData 3. DoReadData 4. DoUpdateData 5. DoDeleteData 6. GetFormActions

--- Forms/SMBBaseMDIChild.dfm
+++ Forms/SMBBaseMDIChild.dfm
@@ -2,9 +2,12 @@ inherited SMBBaseMDIChild: TSMBBaseMDIChild
   Caption = 'SMBBaseMDIChild'
   FormStyle = fsMDIChild
   KeyPreview = True
+  Menu = mmBase
   Visible = True
   OnClose = FormClose
   OnKeyDown = FormKeyDown
+  ExplicitWidth = 434
+  ExplicitHeight = 340
   PixelsPerInch = 96
   TextHeight = 13
   object plBottomButtons: TPanel
@@ -24,10 +27,92 @@ inherited SMBBaseMDIChild: TSMBBaseMDIChild
       Top = 1
       Width = 75
       Height = 25
+      Action = aClose
       Align = alRight
-      Caption = #1047#1072#1082#1088#1099#1090#1100
       TabOrder = 0
-      OnClick = bnCloseClick
+    end
+  end
+  object alBase: TActionList
+    Left = 32
+    Top = 8
+    object aClose: TAction
+      Caption = #1047#1072#1082#1088#1099#1090#1100
+      ShortCut = 27
+      OnExecute = aCloseExecute
+    end
+    object aCreate: TAction
+      Caption = #1044#1086#1073#1072#1074#1080#1090#1100
+      ShortCut = 45
+      OnExecute = aCreateExecute
+    end
+    object aRead: TAction
+      Caption = #1054#1090#1082#1088#1099#1090#1100
+      ShortCut = 113
+      OnExecute = aReadExecute
+    end
+    object aUpdate: TAction
+      Caption = #1057#1086#1093#1088#1072#1085#1080#1090#1100
+      ShortCut = 16467
+      OnExecute = aUpdateExecute
+    end
+    object aDelete: TAction
+      Caption = #1059#1076#1072#1083#1080#1090#1100
+      ShortCut = 8238
+      OnExecute = aDeleteExecute
+    end
+  end
+  object mmBase: TMainMenu
+    Left = 72
+    Top = 8
+    object mmnActions: TMenuItem
+      Caption = #1044#1077#1081#1089#1090#1074#1080#1103
+      GroupIndex = 1
+      object mmnRead: TMenuItem
+        Action = aRead
+      end
+      object mmnSepatator1: TMenuItem
+        Caption = '-'
+      end
+      object mmnCreate: TMenuItem
+        Action = aCreate
+      end
+      object mmnUpdate: TMenuItem
+        Action = aUpdate
+      end
+      object mmnDelete: TMenuItem
+        Action = aDelete
+      end
+      object mmnSepatator2: TMenuItem
+        Caption = '-'
+      end
+      object mmnClose: TMenuItem
+        Action = aClose
+      end
+    end
+  end
+  object pmBase: TPopupMenu
+    Left = 160
+    Top = 96
+    object pmnRead: TMenuItem
+      Action = aRead
+    end
+    object pmnSeparator1: TMenuItem
+      Caption = '-'
+    end
+    object pmnCreate: TMenuItem
+      Action = aCreate
+    end
+    object pmnUpdate: TMenuItem
+      Action = aUpdate
+    end
+    object pmnDelete: TMenuItem
+      Action = aDelete
+    end
+    object pmnSeparator2: TMenuItem
+      Caption = '-'
+    end
+    object pmnClose: TMenuItem
+      Action = aClose
     end
   end
 end
--- Forms/SMBBaseMDIChild.pas
+++ Forms/SMBBaseMDIChild.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, SMBBaseForm, Vcl.StdCtrls, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, SMBBaseForm, Vcl.StdCtrls, Vcl.ExtCtrls,
+  System.Actions, Vcl.ActnList, Vcl.Menus;
 
 type
   TSMBBaseMDIChildClass = class of TSMBBaseMDIChild;
@@ -12,18 +13,108 @@ type
   TSMBBaseMDIChild = class(TSMBBaseForm)
     plBottomButtons: TPanel;
     bnClose: TButton;
-    procedure bnCloseClick(Sender: TObject); virtual;
+    alBase: TActionList;
+    aClose: TAction;
+    aCreate: TAction;
+    aRead: TAction;
+    aUpdate: TAction;
+    aDelete: TAction;
+    mmBase: TMainMenu;
+    mmnActions: TMenuItem;
+    mmnCreate: TMenuItem;
+    mmnRead: TMenuItem;
+    mmnUpdate: TMenuItem;
+    mmnDelete: TMenuItem;
+    mmnSepatator2: TMenuItem;
+    mmnClose: TMenuItem;
+    pmBase: TPopupMenu;
+    mmnSepatator1: TMenuItem;
+    pmnRead: TMenuItem;
+    pmnSeparator1: TMenuItem;
+    pmnCreate: TMenuItem;
+    pmnUpdate: TMenuItem;
+    pmnDelete: TMenuItem;
+    pmnClose: TMenuItem;
+    pmnSeparator2: TMenuItem;
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
     procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure aCloseExecute(Sender: TObject);
+    procedure aCreateExecute(Sender: TObject);
+    procedure aReadExecute(Sender: TObject);
+    procedure aUpdateExecute(Sender: TObject);
+    procedure aDeleteExecute(Sender: TObject);
+  private
+    FCanCreateData: Boolean;
+    FCanReadData: Boolean;
+    FCanUpdateData: Boolean;
+    FCanDeleteData: Boolean;
+    procedure InitMenus;
+  protected
+    procedure DoCloseForm; virtual;
+    procedure DoCreateData; virtual;
+    procedure DoReadData; virtual;
+    procedure DoUpdateData; virtual;
+    procedure DoDeleteData; virtual;
+    procedure GetFormActions(out CanCreateData, CanReadData, CanUpdateData, CanDeleteData: Boolean); virtual;
+  public
+    constructor Create(AOwner: TComponent); override;
   end;
 
 implementation
 
 {$R *.dfm}
 
-procedure TSMBBaseMDIChild.bnCloseClick(Sender: TObject);
+procedure TSMBBaseMDIChild.aCloseExecute(Sender: TObject);
+begin
+  DoCloseForm;
+end;
+
+procedure TSMBBaseMDIChild.aCreateExecute(Sender: TObject);
+begin
+  DoCreateData;
+end;
+
+procedure TSMBBaseMDIChild.aDeleteExecute(Sender: TObject);
+begin
+  DoDeleteData;
+end;
+
+procedure TSMBBaseMDIChild.aReadExecute(Sender: TObject);
+begin
+  DoReadData;
+end;
+
+procedure TSMBBaseMDIChild.aUpdateExecute(Sender: TObject);
+begin
+  DoUpdateData;
+end;
+
+constructor TSMBBaseMDIChild.Create(AOwner: TComponent);
+begin
+  inherited;
+  GetFormActions(FCanCreateData, FCanReadData, FCanUpdateData, FCanDeleteData);
+  InitMenus;
+end;
+
+procedure TSMBBaseMDIChild.DoCloseForm;
 begin
   Close;
+end;
+
+procedure TSMBBaseMDIChild.DoCreateData;
+begin
+end;
+
+procedure TSMBBaseMDIChild.DoDeleteData;
+begin
+end;
+
+procedure TSMBBaseMDIChild.DoReadData;
+begin
+end;
+
+procedure TSMBBaseMDIChild.DoUpdateData;
+begin
 end;
 
 procedure TSMBBaseMDIChild.FormClose(Sender: TObject; var Action: TCloseAction);
@@ -36,6 +127,28 @@ procedure TSMBBaseMDIChild.FormKeyDown(Sender: TObject; var Key: Word;
 begin
   if Key = VK_ESCAPE then
     Close;
+end;
+
+procedure TSMBBaseMDIChild.GetFormActions(out CanCreateData, CanReadData,
+  CanUpdateData, CanDeleteData: Boolean);
+begin
+  CanCreateData := False;
+  CanReadData   := False;
+  CanUpdateData := False;
+  CanDeleteData := False;
+end;
+
+procedure TSMBBaseMDIChild.InitMenus;
+begin
+  aCreate.Visible := FCanCreateData;
+  aUpdate.Visible := FCanUpdateData;
+  aDelete.Visible := FCanDeleteData;
+  aRead.Visible   := FCanReadData;
+  if not FCanReadData then
+  begin
+    mmnSepatator1.Visible := False;
+    pmnSeparator1.Visible := False;
+  end;
 end;
 
 end.
